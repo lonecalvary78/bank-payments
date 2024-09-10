445a12d45e765680f893538140321d617dc012c3
Гунарді Сутанто <lonecalvary78.de@gmail.com>
2024-09-10 12:21:35 +0800

    Add payment-service-discovery

---
+++ payment-service-discovery/src/test/java/demo/app/bank/payment/PaymentServiceDiscoveryApplicationTest.java
@@ -0,0 +1,10 @@
+package demo.app.bank.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PaymentServiceDiscoveryApplicationTest {
+  @Test
+  public void contextLoads() {}
+}
